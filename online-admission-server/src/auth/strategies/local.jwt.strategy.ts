bf3efb691b4373d646099b77354279f47ace6bfd
George Acquah <georgeacquah889@gmail.com>
2023-09-17 03:18:47 +0000

    Added JWT authentication

---
+++ online-admission-server/src/auth/strategies/local.jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { CurrentUser } from '../shared/types/user';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class LocalJwtStrategy extends PassportStrategy(Strategy, 'local') {
+  constructor(private userService: UserService) {
+    super({ usernameField: 'username' });
+  }
+
+  async validate(username: string, password: string): Promise<CurrentUser> {
+    const user = await this.userService.validateUserCredentialsByUsername(
+      username,
+      password,
+    );
+
+    if (!user) {
+      console.log('I wont aunthenticate you!!!');
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
---
+++ online-admission-server/src/shared/interfaces/jwt.payload.ts
@@ -0,0 +1,6 @@
+export interface Payload {
+  email: string;
+  username?: string;
+  iat?: number;
+  expiresIn?: number;
+}

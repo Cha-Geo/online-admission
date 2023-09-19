03cd796af3ba9e745331453ea788163d6418e976
George Acquah <georgeacquah889@gmail.com>
2023-09-19 05:06:24 +0000

    Modified navbar to enhance usage of section smooth scrolling

--- online-admission-client/components/ui/Navbar.tsx
+++ online-admission-client/components/ui/Navbar.tsx
@@ -15,15 +15,19 @@ const Navbar = () => {
     setOpenDropdown((prevItem) => (prevItem === itemName ? "" : itemName));
   };
 
-  const { navigationItems, aboutData } = navLinks;
+  const closeDropdown = () => {
+    setOpenDropdown("");
+  }
+
+  const { navigationItems } = navLinks;
   return (
     <header className="absolute inset-x-0 top-0 z-50">
       <nav
-        className="flex items-center justify-between p-6 lg:px-8"
+        className="flex items-center justify-between p-6 lg:px-8 text-2xl"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <Link href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <Image
               width={20}
@@ -32,29 +36,69 @@ const Navbar = () => {
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt=""
             />
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <IconAllignLeftMenu
-            className="-m-2.5 w-12 font-bold p-2.5 text-gray-700 cursor-pointer hover:shadow-sm rounded-full"
+            className="-m-2.5 w-12 font-bold p-2.5 text-gray-700 cursor-pointer hover:shadow-sm rounded-full max"
             onClick={() => setMobileMenuOpen(true)}
           />
         </div>
 
         <div className="hidden lg:flex lg:gap-x-12">
           {navigationItems.map((item) => (
-            <Link
+            <div
               key={item.name}
-              href={item.path}
-              className="text-sm font-semibold leading-6 text-gray-900"
+              onMouseEnter={() => handleDropdown(item.name)}
+              onMouseLeave={closeDropdown}
+              className="relative"
             >
-              {item.name}
-            </Link>
+              {" "}
+              <div className="flex items-center gap-2">
+                <Link
+                  key={item.name}
+                  href={item.path}
+                  className="text-base font-semibold hover:font-bold leading-6 text-gray-900"
+                  onClick={closeDropdown}
+                >
+                  {item.name}
+                </Link>
+                {item.data && ( // Updated condition to check isOpen
+                  <div className="flex items-center">
+                    {openDropdown === item.name ? (
+                      <IconArrowUp
+                        onClick={() => handleDropdown(item.name)}
+                        className="cursor-pointer"
+                      />
+                    ) : (
+                      <IconArrowDown
+                        onClick={() => handleDropdown(item.name)}
+                        className="cursor-pointer"
+                      />
+                    )}
+                  </div>
+                )}
+              </div>
+              {!mobileMenuOpen && openDropdown === item.name && item.data && (
+                <div className="py-2 px-2 transition-all duration-500 font-semibold dropdown custom-dropdown">
+                  {item.data.map((data, j) => (
+                    <Link
+                      key={j}
+                      className="px-4 py-2   gap-5 text-gray-900 hover:bg-gray-50 leading-7 rounded-lg text-base text-start w-full "
+                      href={`${item.path}?section=${data.id as string}`}
+                      onClick={() => setMobileMenuOpen(false)}
+                    >
+                      <div className="">{data.name}</div>
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
           ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
+          <a href="/" className="outline_btn hover:bg-opacity-60 ">
+            Apply Now
           </a>
         </div>
       </nav>
@@ -63,7 +107,7 @@ const Navbar = () => {
           <div className="fixed inset-0 z-50" />
           <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between font-semibold">
-              <a href="#" className="-m-1.5 p-1.5">
+              <Link href="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <Image
                   width={20}
@@ -72,7 +116,7 @@ const Navbar = () => {
                   src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                   alt=""
                 />
-              </a>
+              </Link>
               <button
                 type="button"
                 className="-m-2.5 w-10 p-2.5 text-gray-700 text-lg hover:shadow-sm shadow-slate-600 rounded-full"
@@ -139,12 +183,13 @@ const Navbar = () => {
                   </div>
                 </div>
                 <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  <Link
+                    href="/"
+                    className="-mx-3 mt-10 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 black_btn"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
-                    Log in
-                  </a>
+                    Apply Now
+                  </Link>
                 </div>
               </div>
             </div>

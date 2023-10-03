0c9a2a50513466a8766b0c6430cf3627279a233f
George Acquah <georgeacquah889@gmail.com>
2023-10-03 00:50:09 +0000

    New updates to client

--- online-admission-client/app/page.tsx
+++ online-admission-client/app/page.tsx
@@ -1,20 +1,30 @@
-import { fetchPosts } from '@/services/dataFetching';
+import { Button } from '@/components/button';
+import { fetchPosts, fetchUsers } from '@/services/dataFetching';
 
 type Props = {}
 
 const Home = async (props: Props) => {
   const programs: IPosts[] = await fetchPosts();
+  const users: any = await fetchUsers();
 
   return (
-    <div className='text-lg font-semibold '>
+    <div className="text-lg font-semibold ">
       {/* <Hero2 /> */}
-      {programs?.slice(0,10).map(program => (
+      <Button>Click Me</Button>
+      
+      {/* {users && users.map((user) => (
+        <p key={user?.id}>
+          {user?.username}, {user?.role}{" "}
+        </p>
+      ))} */}
+
+      {programs?.slice(0, 10).map((program) => (
         <div key={program.id} className="">
-          <h2 className='py-4 my-4'>{program.title}</h2>
+          <h2 className="py-4 my-4">{program.title}</h2>
         </div>
       ))}
     </div>
-  )
+  );
 }
 
 export default Home;
